66e91ac1b3267327916ff6529ff9dcb45dba302d
Priyanshuuuu <167881010+Bre4d77@users.noreply.github.com>
2024-11-08 09:29:54 +0530

    Delete src directory

--- src/Structures/index.ts
+++
@@ -1,8 +0,0 @@
-import BaseClient from './BaseClient.structure.js';
-import { Command } from './Command.structure.js';
-import Context from './Context.structure.js';
-import Event from './Event.structure.js';
-
-
-
-export { BaseClient, Command, Event, Context };
